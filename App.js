266cd7319090feacfdac8a55a0f028826f5d328f
RAHUL MISHRA <rahulmishra2370@gmail.com>
2024-01-04 17:20:58 +0530

    first version of my background changer app on press

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
+
+  const [randomBackground,setRandomBackground]=useState("#FFFFFF")
+
+  const generateColor = ()=>{
+    const hexRange="0123456789"
+    let color = "#"
+
+    for (let i=0;i<6;i++){
+      color += hexRange[Math.floor(Math.random() * 16)]
+    }
+
+    setRandomBackground(color)
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style={[styles.container,{backgroundColor:randomBackground}]}>
+   <TouchableOpacity onPress={generateColor}>
+   <View style={styles.actionbtn}>
+      <Text style={styles.actionbtntxt}>press me</Text>
+    </View>
+    </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+     container:{
+      flex:1,
+      alignItems:'center',
+      justifyContent:'center'
+     },
+     actionbtn:{
+      borderRadius:12,
+      backgroundColor:"#6A1B4D",
+      paddingHorizontal:40,
+      paddingVertical:10
+     },
+     actionbtntxt:{
+      fontSize:24,
+      color:"#FFFFFF",
+
+     }
+})
